600ff6e9ba5e5b6cf96dace5ad8ba479e2ee4b91
Colin Bull <colinbul@googlemail.com>
2015-09-14 06:29:49 +0100

    Added misc autoload and customisd pretty mode

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 
 (setq inhibit-splash-screen t)
 (setq-default truncate-lines 1)
+(toggle-frame-fullscreen)
 ;;------------------------------------------------------------------------------
 ;; System type discriminators
 ;;------------------------------------------------------------------------------
@@ -64,15 +65,16 @@
     fsharp-mode
     omnisharp
     gist
-   ;; haskell-mode
+    haskell-mode
     idris-mode
     json
     markdown-mode
     rainbow-delimiters
     yasnippet
-    pretty-mode
+ ;;   pretty-mode
     projectile
     flx-ido
+    ace-jump-mode
     )
   "A list of packages to ensure are installed at launch.")
 
@@ -83,10 +85,10 @@
 (eval-after-load "sql"
   '(load-library "tsql-indent"))
 
+
 ;;------------------------------------------------------------------------------
 ;; General settings
 ;;------------------------------------------------------------------------------
-
 (setq debug-on-error t)
 
 ;set theme
@@ -110,6 +112,7 @@
 ;; Keyboard
 ;;------------------------------------------------------------------------------
 
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 ;; scroll from the keyboard (what is everyone else using!?)
 (global-set-key "\M-N" 'up-semi-slow)
 (global-set-key "\M-P" 'down-semi-slow)
@@ -119,6 +122,18 @@
 (global-set-key (kbd "C-x <down>") 'windmove-down)
 (global-set-key (kbd "C-x <left>") 'windmove-left)
 (global-set-key (kbd "C-x <right>") 'windmove-right)
+
+(autoload 'zap-up-to-char "misc"
+  "Kill up to, but not including ARGth occurrence of CHAR.")
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+;;-----------------------------------------------------------------------------
+;; ACE Jump
+;;-----------------------------------------------------------------------------
+
+(require 'ace-jump-mode)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
 
 ;;------------------------------------------------------------------------------
 ;; IDO
@@ -204,7 +219,7 @@
 (require 'idris-mode)
 
 ;;Point at idris dev sandbox
-(add-to-list 'exec-path "~/Appdev/idris/.cabal-sandbox/bin")
+(add-to-list 'exec-path "~/.cabal/bin")
 
 
 ;;------------------------------------------------------------------------------
@@ -277,7 +292,85 @@
 ;; Haskell
 ;;------------------------------------------------------------------------------
 
-;;(require 'haskell-mode-autoloads)
+(add-to-list 'load-path ".")
+;; Always load via this. If you contribute you should run `make all`
+;; to regenerate this.
+(load "haskell-mode-autoloads")
+
+;; Customization
+(custom-set-variables
+ ;; Use cabal-dev for the GHCi session. Ensures our dependencies are in scope.
+ ;;'(haskell-process-type 'cabal-dev)
+
+ ;; Use notify.el (if you have it installed) at the end of running
+ ;; Cabal commands or generally things worth notifying.
+ '(haskell-notify-p t)
+
+ ;; To enable tags generation on save.
+ '(haskell-tags-on-save t)
+
+ ;; To enable stylish on save.
+ '(haskell-stylish-on-save t))
+
+(add-hook 'haskell-mode-hook 'haskell-hook)
+(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
+
+;; Haskell main editing mode key bindings.
+(defun haskell-hook ()
+  ;; Use simple indentation.
+  (turn-on-haskell-simple-indent)
+  (define-key haskell-mode-map (kbd "<return>") 'haskell-simple-indent-newline-same-col)
+  (define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
+
+  ;; Load the current file (and make a session if not already made).
+  (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
+  (define-key haskell-mode-map [f5] 'haskell-process-load-file)
+
+  ;; Switch to the REPL.
+  (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
+  ;; “Bring” the REPL, hiding all other windows apart from the source
+  ;; and the REPL.
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+
+  ;; Build the Cabal project.
+  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  ;; Interactively choose the Cabal command to run.
+  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+  ;; Get the type and info of the symbol at point, print it in the
+  ;; message buffer.
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+
+  ;; Contextually do clever things on the space key, in particular:
+  ;;   1. Complete imports, letting you choose the module name.
+  ;;   2. Show the type of the symbol after the space.
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+
+  ;; Jump to the imports. Keep tapping to jump between import
+  ;; groups. C-u f8 to jump back again.
+  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+
+  ;; Jump to the definition of the current symbol.
+  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
+
+  ;; Indent the below lines on columns after the current column.
+  (define-key haskell-mode-map (kbd "C-<right>")
+    (lambda ()
+      (interactive)
+      (haskell-move-nested 1)))
+  ;; Same as above but backwards.
+  (define-key haskell-mode-map (kbd "C-<left>")
+    (lambda ()
+      (interactive)
+      (haskell-move-nested -1))))
+
+;; Useful to have these keybindings for .cabal files, too.
+(defun haskell-cabal-hook ()
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
 
 ;;------------------------------------------------------------------------------
 
